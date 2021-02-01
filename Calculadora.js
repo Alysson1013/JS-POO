ffa7170f59061ba75751bdc54922bdcdf31df68d
Alysson Eduardo <alysson6801@hotmail.com>
2021-02-01 17:14:54 -0300

    (01/02) Métodos estáticos.

---
+++ Calculadora.js
@@ -0,0 +1,12 @@
+class Calculadora{
+    static Somar(a,b){
+        console.log(a+b)
+    }
+    static Sub(a,b){
+        console.log(a-b)
+    }
+}
+
+Calculadora.Somar(15, 2)
+Calculadora.Sub(15, -2)
+
