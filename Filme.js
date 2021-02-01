6372127567c1753b6e7996bc73061e36f1fe114c
Alysson Eduardo <alysson6801@hotmail.com>
2021-02-01 16:09:18 -0300

    (01/02) Classes em Javascript.

---
+++ Filme.js
@@ -0,0 +1,26 @@
+//Classes em Javascript
+class Filme{
+    //Construtor em JS
+    constructor(){
+        this.titulo = '';
+        this.ano = 0;
+        this.genero = '';
+        this.diretor = '';
+        this.atores = [];
+        this.duracao = 0;
+    }
+
+    //Métodos
+    Reproduzir(){
+        console.log("assistindo")
+    }
+    Pausar(){
+        console.log("Pausado ||")
+    }
+    Avancar(){
+        console.log("Avançar >>")
+    }
+    Fechar(){
+        console.log("Fechar X")
+    }
+}
